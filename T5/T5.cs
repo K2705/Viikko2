ad8358c75eb1128bc2a452ec560b29a1eac4df51
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-11 17:45:54 +0200

    Teht5

---
+++ T5/T5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T5
+{
+    class T5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna sekunnit\n> ");
+            int input = 0;
+            int.TryParse(Console.ReadLine(), out input);
+
+            int s = input % 60;
+
+            int min = (input % (60 * 60)) / 60;
+
+            int h = input / (60 * 60);
+
+            Console.WriteLine("Aika on " + h + " tuntia, " + min + " minuuttia ja " + s + " sekuntia.");
+        }
+    }
+}
