b2c0390df4e07bbd2512414d9dc0e13f6179de2f
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-11 18:47:17 +0200

    T10

---
+++ T10/T10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T10
+{
+    class T10
+    {
+        /// <summary>
+        /// Prints a hardcoded string every time a number in a hardcoded array is even.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            const String HEPTEXT = "HEP";
+            int[] luvut = { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
+            for (int i = 0; i < luvut.Length; i++)
+            {
+                if (luvut[i] % 2 == 0) Console.WriteLine(HEPTEXT);
+            }
+        }
+    }
+}
