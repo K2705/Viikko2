61ddb15ebc870769446fc7bcc9a5893b708dac82
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-11 18:40:29 +0200

    tehtävät 8 ja 9

---
+++ T8/T8.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace T8
+{
+    class T8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna luku\n> ");
+            int n1 = 0;
+            int.TryParse(Console.ReadLine(), out n1);
+            Console.Write("Toinen luku\n> ");
+            int n2 = 0;
+            int.TryParse(Console.ReadLine(), out n2);
+            Console.Write("Kolmas luku\n> ");
+            int n3 = 0;
+            int.TryParse(Console.ReadLine(), out n3);
+
+            Console.WriteLine("Suurin luku on " + SuurinKolmesta(n1, n2, n3));
+        }
+
+        /// <summary>
+        /// Palauttaa kolmesta luvusta suurimman.
+        /// Jos luvut ovat yhtä suuria, niistä palautetaan ensimmäinen.
+        /// </summary>
+        /// <param name="n1">1. luku</param>
+        /// <param name="n2">2. luku</param>
+        /// <param name="n3">3. luku</param>
+        /// <returns>luvuista suurin</returns>
+        private static int SuurinKolmesta(int n1, int n2, int n3)
+        {
+            int suurin = n1;
+            if (n2 > suurin) suurin = n2;
+            if (n3 > suurin) suurin = n3;
+            return suurin;
+        }
+    }
+}
---
+++ T9/T9.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+
+namespace T9
+{
+    /// <summary>
+    /// kysyy käyttäjältä lukuja, kunnes hän syöttää luvun 0.
+    /// tulostaa syötettyjen lukujen summan.
+    /// </summary>
+    class T9
+    {
+        static void Main(string[] args)
+        {
+            ArrayList luvut = new ArrayList();
+            int input = 1; //mielivaltainen arvo joka ei ole 0
+            do
+            {
+                Console.Write("Anna luku (0 lopettaa)\n>");
+                if (int.TryParse(Console.ReadLine(), out input))
+                {
+                    luvut.Add(input);
+                }
+                else
+                {
+                    Console.WriteLine("Tuo ei ole luku.");
+                    input = 1; //mielivaltainen arvo joka ei ole 0
+                }
+            } while (input != 0);
+
+            int summa = 0;
+            foreach (int luku in luvut)
+            {
+                summa += luku;
+            }
+
+            Console.WriteLine("Lukujen summa on " + summa);
+        }
+    }
+}
