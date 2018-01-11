4eae6b636326bbb7f171b351c95387686ebf4fec
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-11 17:27:50 +0200

    teht4

---
+++ T4/T4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4
+{
+    class T4
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna ikä\n> ");
+            int n = 0;
+            int.TryParse(Console.ReadLine(), out n);
+
+            if (n < 18) Console.WriteLine("\nAlaikäinen");
+            if (n >= 18 && n < 65) Console.WriteLine("\nAikuinen");
+            if (n >= 65) Console.WriteLine("\nSeniori");
+        }
+    }
+}
