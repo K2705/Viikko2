8f6bca2e9e30fc87237327fb0656129d1738d1d9
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-11 17:14:47 +0200

    teht2

--- T2/T2.cs
+++ T2/T2.cs
@@ -10,6 +10,23 @@ namespace T2
     {
         static void Main(string[] args)
         {
+            Console.Write("Montako pistettä?\n> ");
+            int n = 0;
+            int.TryParse(Console.ReadLine(), out n);
+
+            if (n < 0 || n >= 13) Console.Write("Eikä ole.");
+            else
+            {
+                Console.Write("\nNumeroksi tulee ");
+                if (n >= 0 && n < 2) Console.Write("0");
+                if (n >= 2 && n < 4) Console.Write("1");
+                if (n >= 4 && n < 6) Console.Write("2");
+                if (n >= 6 && n < 8) Console.Write("3");
+                if (n >= 8 && n < 10) Console.Write("4");
+                if (n >= 10 && n < 13) Console.Write("5");
+            }
+            Console.WriteLine();
         }
     }
 }
+
