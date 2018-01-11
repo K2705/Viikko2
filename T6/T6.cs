3c661b8452c4cb2fae1c2ea8a69b190287c3dc35
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-01-11 18:22:25 +0200

    tehtävät 6 ja 7

---
+++ T6/T6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    class T6
+    {
+        static void Main(string[] args)
+        {
+            const double GAS_PRICE = 1.595;
+            const double GAS_CONSUMPTION = 7.02 / 100;
+
+            Console.Write("Anna matka\n> ");
+            double n = 0;
+            double.TryParse(Console.ReadLine(), out n);
+
+            double gasUse = GAS_CONSUMPTION * n;
+            double gasCost = gasUse * GAS_PRICE;
+
+            Console.WriteLine("\nBensaa kuluu " + gasUse + " litraa ja se maksaa " + gasCost + " euroa.");
+        }
+    }
+}
---
+++ T7/T7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7
+{
+    class T7
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna vuosi\n> ");
+            int n = 0;
+            int.TryParse(Console.ReadLine(), out n);
+
+            bool karkausvuosi = false;
+            if ((n % 4) == 0) karkausvuosi = true;
+            if ((n % 100) == 0) karkausvuosi = false;
+            if ((n % 400) == 0) karkausvuosi = true;
+
+            Console.WriteLine("Vuosi " + n + (karkausvuosi ? " on karkausvuosi." : " ei ole karkausvuosi."));
+        }
+    }
+}
